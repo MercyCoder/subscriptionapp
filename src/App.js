5cfc5b9c17db5600e89a673239a9d6024c4e3dd0
Mercy <mercyemeth@gmail.com>
2020-01-05 12:06:53 -0500

    initial commit and displays members with subscription sorted by price

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import { element } from 'prop-types';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.displaySubscriptions();
+    this.state = {
+      members: [],
+      sortedSubscriptions: []
+    };
+  };
+
+  displaySubscriptions = () => {
+    let abortController = new AbortController();
+    axios.get(
+      "http://localhost:3000/members"
+    )
+    .then(response => {
+      if (response && response.data) {
+        console.log("this is the data! ", response.data )
+        this.setState({ members: response.data});
+        this.displayByPrice()
+      }
+    })
+
+    .catch(error => {
+      console.log("***** error: " , error);
+    });
+    abortController.abort();
+  }
+  displayByPrice = () => {
+    let tempArray = [].concat(this.state.members).sort((a, b) => a.price > b.price ? -1 : 1);
+    this.setState({sortedSubscriptions: tempArray})
+    console.log("this is sorted subscriptions!", this.state.sortedSubscriptions)
+    console.log("this is the temp array", tempArray)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        </header>
+        <div>Members</div>
+        <div>Members:  {this.state.members.map(element => element.name)}
+          <ol>{this.state.sortedSubscriptions.map((element, index) => { return <div key={index}>{element.name} {element.id} {element.subscription} {element.price}</div>})}</ol>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
